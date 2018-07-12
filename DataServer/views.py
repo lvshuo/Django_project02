8d0c5223632c6616f799f5bb46642785cba7544a
GOERTEK\andy.lv <andy.lv@goertek.com>
2018-07-12 14:01:02 +0800

    add oracle select data with paras

--- DataServer/views.py
+++ DataServer/views.py
@@ -12,6 +12,20 @@ from rest_framework.request import Request
 from rest_framework import exceptions
 import datetime
 # Create your views here.
+'''
+  ORG_CODE:
+  
+    63878A393AA041CBB34F30DEDFE2619B  ## 科技园  SMT制造一部
+  
+  PROJECT_ID:
+  
+    ALMOND 
+
+'''
+
+
+
+
 class dbreadrest(APIView):
    # authentication_classes = [TestAuthentication, ]
     #permission_classes = []
@@ -53,33 +67,69 @@ class dbreadrest01(APIView):
 def logindb(request):
 
     data =json.loads(request.body)
-    #print(data['data1'])
+    print(data)
+    #condition=data["condition"]
+    #print(condition)
     data_test= {"data1": " ",
                 "data2": {"data": []
                          }
                 }
     data_to_send={"status": "400", "msg": "No User", "data": "null" }
-    print(data)
+    #print(data)
     conn2 = cx_Oracle.connect('BJYJY', 'abc@123', '10.10.31.115:1521/orcl')
     #
     cursor2 = conn2.cursor()
     #
-    cursor2.execute('select * from OLE_DB.CT_STANDARD_BEST')
-    result=cursor2.fetchone()
-    data1=result[3]
-    date_test=datetime.datetime.now()
-    print(date_test)
-    print(date_test.month)
-    print(data1)
-    #cursor2.execute('select * from OLE_DB.RPT_LINE_DAILY_L where WORK_DATE =')
-    cursor2.execute("select * from OLE_DB.RPT_LINE_DAILY_L where WORK_DATE =TO_DATE('2018-07-06', 'YYYY-MM-DD')")
-    #cursor2.execute("select * from OLE_DB.RPT_LINE_DAILY_L where  date_format(WORK_DATE ,'%Y-%m-%d') ='2018-07-06'")
+    # cursor2.execute('select * from OLE_DB.CT_STANDARD_BEST')
+    # result=cursor2.fetchone()
+    # data1=result[3]
+    # date_test=datetime.datetime.now()
+    # print(date_test)
+    # print(date_test.month)
+    # print(data1)
+    #cursor2.execute('select * from OLE_DB.RPT_LINE_DAILY_L where WORK_DATE =')  WORK_DATE AND
+    # cursor2.execute("select *   from OLE_DB.RPT_LINE_DAILY_L where WORK_DATE  between TO_DATE('2018-07-04', 'YYYY-MM-DD') and TO_DATE('2018-07-06', 'YYYY-MM-DD')"
+    #                 "AND  PROJECT_ID = 'ALMOND'"
+    #                 "AND SHIFT_TYPE ='DAY' "
+    #                 "AND IS_STANDARD = 'Y' "
+    #               )
+
+    try:
+        cursor2.execute("select CIRCLE_LOSS  from OLE_DB.RPT_LINE_DAILY_L where WORK_DATE  between TO_DATE(:START_TIME, 'YYYY-MM-DD') and TO_DATE(:END_TIME, 'YYYY-MM-DD') "
+                     "AND ORG_CODE = :ORG_CODE"
+                     " AND PROJECT_ID = :PROJECT_ID"                  
+                     " AND IS_STANDARD = :IS_STANDARD "
+                     "AND  SERIES = :SERIES"
+                     " AND LINE_CODE = :LINE_CODE", data
+                     )
+
+
+    except cx_Oracle.DatabaseError as e:
+        res = {"status": "400", "msg": "No Data", "data": "null"}
+        return HttpResponse(json.dumps(res))
+
+    try:
+        cursor2.execute(
+            "select LINE_END_LOSS  from OLE_DB.RPT_LINE_DAILY_L where WORK_DATE  between TO_DATE(:START_TIME, 'YYYY-MM-DD') and TO_DATE(:END_TIME, 'YYYY-MM-DD') "
+            "AND ORG_CODE = :ORG_CODE"
+            " AND PROJECT_ID = :PROJECT_ID"
+            " AND IS_STANDARD = :IS_STANDARD "
+            "AND  SERIES = :SERIES"
+            " AND LINE_CODE = :LINE_CODE", data
+        )
+
+    except cx_Oracle.DatabaseError as e:
+        res = {"status": "400", "msg": "No Data", "data": "null"}
+        return HttpResponse(json.dumps(res))
+
+    #    "AND PN_CODE ='TRACKERX' "  PRODUCT_OUT_QTY  (condition_project["PROJECT_ID"], condition_project["SHIFT_TYPE"], condition_project["IS_STANDARD"]
+    #cursor2.execute("select * from OLE_DB.RPT_LINE_DAILY_L where  date_format(WORK_DATE ,'%Y-%m-%d') ='2018-07-06'")    " AND SHIFT_TYPE = :SHIFT_TYPE "
     result = cursor2.fetchall()
     #result = cursor2.fetchmany(10)
     #
    # print(result.rowcount)
     #
-    print(result)
+    #print(result)
     a=[1,2,3,4,5]
     data_test = {"data1": " ",
                  "data2": {"data": a
